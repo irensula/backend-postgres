ff83ac5a46737477361b18ea1971139e26f78be2
irensula <irinawebdev@gmail.com>
2026-07-01 19:12:05 +0300

    fix user router

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -4,31 +4,25 @@ const config = require("../utils/config");
 const knex = require("knex")(config.DATABASE_OPTIONS);
 const bcrypt = require("bcryptjs");
 // get user's data
-router.get("/:id", async (req, res) => {
-  try {
-    const userId = res.locals.auth.userId;
-    const user = await knex("users")
-      .where({ "users.user_id": userId })
-      .leftJoin("avatars", "users.avatar_id", "avatars.avatar_id")
-      .select(
-        "users.user_id",
-        "users.username",
-        "users.email",
-        "users.avatar_id",
-        "users.ui_language_id",
-        "avatars.avatar_path"
-      )
-      .first();
+router.get("/me", async (req, res) => {
+  const userId = res.locals.auth.userId;
 
-      if (!user) {
-        res.status(404).json({ error: "user not found" });
-      }
-      return res.json(user);
-  } catch (error) {
-    console.error("Error fetching user: ", error.message);
-    res.status(500).json({ error: "Failed to fetch user by _id" });
-  }
-});
+  const user = await knex("users")
+    .where({ user_id: userId })
+    .first()
+    .select(
+      "user_id",
+      "username",
+      "email",
+      "avatar_id",
+      "ui_language_id"
+    );
+
+  const courses = await knex("users_languages")
+    .where({ user_id: userId });
+  
+  res.json({ user, courses });
+})
 // edit user's data
 router.put("/:id", async (req, res) => {
   try {
